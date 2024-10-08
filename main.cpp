b8bb953b994fa4d1aba7720609eaaafb8ebde4d1
Ryan Omoruyi <ryan-o@Ryans-MacBook-Pro.local>
2024-10-07 21:28:48 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    // create a socket
+    // biind socket to an IP / port
+    // mark socket for listening for connections
+    // accept connections
+    // close socket
+    // when receiving,  displaay msg, echo msg
+    // close socket
+    return 0;
+}
