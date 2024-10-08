5fdafb97b80e73d93d73c6b3d68262fb2d70b581
Ryan Omoruyi <omoruyi.ryan@gmail.com>
2024-10-07 21:39:38 -0400

    add C libs and create socket

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+#include <string>
+
+// .h are from C lib
+
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
     // create a socket
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (!listening) {
+        cerr << "Failed to create socket" << endl;
+        return -1;
+    }
     // biind socket to an IP / port
     // mark socket for listening for connections
     // accept connections
